72150570a602c89b671c3c3df0953b81d925a282
youngkhalid <youngkhalifah98@gmail.com>
2018-07-13 15:54:57 +0300

    js scripts for the if statement done

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,33 @@
-$(document).ready(function() {
-    $("form#results").submit(function (event) {
+//Business Logic//
+ var pingpong = function(ping){
+   for (var i = 0; i <= input1; i++) {
+        if ( (ping% 3 === 0)){
+            console.log("ping");
+        }
+        else if (( ping % 5) === 0){
+            console.log("Pong");
+        }
+    
+        else if ((ping % 3) === 0 && (ping % 5 === 0)) {
+            console.log("Ping Pong");
+        }
+       
+       }
+    }
+    
+
+    
+
+$().ready(function(){
+    $("form#btn1").submit(function (event) {
         event.preventDefault();
-});
-});
+
+        var Number=parseInt($("#input1").val());
+        var result=pingpong(ping);
+        $("#result").text(result)
+
+        console.log("#input1");
+
+    })
+
+})
