1c372ed93837ef35f4ac193cd35c47ba1e3f3ccc
youngkhalid <youngkhalifah98@gmail.com>
2018-07-16 09:14:09 +0300

    title background color changed

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+//User Inteface
+
+$(document).ready(function(){
+    $("form#PingPong").submit(function (event) {
+        event.preventDefault();
+
+      Ping = parseInt($("input#ping").val());
+        $("#result").text(" "); 
+        react();
+        $("input#ping").val();
+
+        
+
+    });
+
+});
+
+//Business Logic
+var Ping;
+function react(){
+    for(var x =1; x <= Ping; x++ ){
+    if ((x % 3) === 0 && (x % 5) === 0) {
+        $("#result").append("<li>Ping Pong</li>");
+    }
+     else if ((x % 3) === 0){
+        $("#result").append("<li>Ping</li>");
+    }
+    else if (( x % 5) === 0){
+        $("#result").append("<li>Pong</li>");
+    }
+
+    else{
+        $("#result").append("<li>"+ x +"</li>");
+    }
+   
+} 
+   
+}
