68f6b233fc7e84b3cc640c7d0184e302c443eef9
youngkhalid <youngkhalifah98@gmail.com>
2018-07-17 08:04:39 +0300

    js changes made to master

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //User Inteface
 
 $(document).ready(function(){
@@ -6,26 +6,14 @@ $(document).ready(function(){
         event.preventDefault();
 
       Ping = parseInt($("input#ping").val());
-        $("#result").text(" "); 
+        $("#result").text(" ");
         react();
         $("input#ping").val();
-=======
-$().ready(function(){
-    $("form#PingPong").submit(function (event) {
-        event.preventDefault();
-
-        var ping=parseInt($("input#ping").val());
-        var result=PingPong(ping);
-        $("#result").text(result);
->>>>>>> 09ff1e6552af9035773b9889cd41af188ea113ed
-
-        
 
     });
 
 });
 
-<<<<<<< HEAD
 //Business Logic
 var Ping;
 function react(){
@@ -43,29 +31,7 @@ function react(){
     else{
         $("#result").append("<li>"+ x +"</li>");
     }
-   
-} 
-   
+
 }
-=======
 
-var PingPong =function(ping){
-    
-    
-    if ((ping % 3) === 0 && (ping % 5) === 0) {
-        return "Ping Pong"
-    }
-     else if ((ping% 3) === 0){
-        return "ping"
-    }
-    else if (( ping % 5) === 0){
-        return "Pong"
-    }
-
-    else{
-        return ping;
-    }
-   
-   
-};
->>>>>>> 09ff1e6552af9035773b9889cd41af188ea113ed
+}
